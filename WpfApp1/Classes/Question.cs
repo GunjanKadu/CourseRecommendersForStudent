063ac203d5459ec0b1b74c9c36e4c3f6d4919284
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-04-23 13:44:09 +0200

    setting up questionare and adding some questions

---
+++ WpfApp1/Classes/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Classes
+{
+    public class Question
+    {
+        public string image { get; set; }
+        public string qType { get; set; }
+        public string qText { get; set; }
+        public int id { get; set; }
+        public List<Answer> answers { get; set; }
+
+        public Question()
+        {
+            answers = new List<Answer>();
+        }
+    }
+
+    public class Answer
+    {
+        public string aText { get; set; }
+        public bool isCorrect { get; set; }
+    }
+}
--- WpfApp1/Questionare.xaml.cs
+++ WpfApp1/Questionare.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Classes;
 
 namespace WpfApp1
 {
@@ -19,14 +20,42 @@ namespace WpfApp1
     /// </summary>
     public partial class Questionare : Window
     {
+        Question question;
         public Questionare()
         {
-            InitializeComponent();
+            InitializeComponent();      
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Owner.Visibility = Visibility.Visible;
+        }
+
+        private void Btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            var res = MessageBox.Show("Are You Sure You Want To Start?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.Yes)
+            {
+                Txt_PressStart.Visibility = Visibility.Hidden;
+                Btn_Start.Visibility = Visibility.Hidden;
+                Lst_AnswerList.Visibility = Visibility.Visible;
+                Txt_Question.Visibility = Visibility.Visible;
+
+
+
+                question = new Question { id = 0, qText = "1).What is Your Current Qualification ?  " };
+                question.answers.Add(new Answer { aText = "High School Graduate", isCorrect = false });
+                question.answers.Add(new Answer { aText = "Bachelor's", isCorrect = false });
+                question.answers.Add(new Answer { aText = "Masters's", isCorrect = true });
+                question.answers.Add(new Answer { aText = "Other", isCorrect = false });
+
+                DataContext = question;
+            }
+
+        }
+
+        private void Lst_AnswerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
